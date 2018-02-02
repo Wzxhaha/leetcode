74ce2da12e1acf05006d8dea06ab477e1b618469
wzxjiang <wzxjiang@foxmail.com>
2018-02-02 16:01:28 +0800

    update two_sum

--- two_sum.go
+++ two_sum.go
@@ -1,10 +1,11 @@
 func twoSum(nums []int, target int) []int {
+	if len(nums) < 2 {
+    	return nil
+    }
 	m := make(map[int]int)
-
 	for index, value := range nums {
 		m[value] = index
 	}
-
 	for index, value := range nums {
 		key := target - value
 		mm, _ := m[key]
@@ -12,6 +13,5 @@ func twoSum(nums []int, target int) []int {
 			return []int{index, mm}
 		}
 	}
-
-	return []int{0, 0}
+	return nil
 }
